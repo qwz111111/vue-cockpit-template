aa2a3deac1025370406391fedd4c408f7736500d
AsusDesktop <2310860596@qq.com>
2024-07-27 12:00:37 +0800

    新增直播播放组件

---
+++ src/components/BaseVideoPlayer/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="base-video-player">
+    <video
+      ref="videoPlayer"
+      class="video-js vjs-default-skin"
+      controls
+      preload="auto"
+    ></video>
+  </div>
+</template>
+
+<script>
+import videojs from 'video.js'
+import 'video.js/dist/video-js.css'
+
+export default {
+  name: 'BaseVideoPlayer',
+  props: {
+    src: {
+      type: String,
+      required: true
+    }
+  },
+  beforeDestroy() {
+    this.playerDispose()
+  },
+  activated() {
+    this.playerPlay()
+  },
+  deactivated() {
+    this.playerPause()
+  },
+  mounted() {
+    this.playerInit()
+  },
+  watch: {
+    src() {
+      this.playerInit()
+    }
+  },
+  methods: {
+    playerInit() {
+      this.playerDispose()
+      this.player = videojs(this.$refs.videoPlayer, {
+        autoplay: true, // 设置自动播放
+        muted: true, // 设置了它为true，才可实现自动播放,同时视频也被静音 （Chrome66及以上版本，禁止音视频的自动播放）
+        preload: 'auto', // 预加载
+        controls: false, // 显示播放的控件
+        sources: [
+          {
+            src: this.src,
+            type: 'application/x-mpegURL'
+          }
+        ]
+      })
+    },
+    playerPause() {
+      if (this.player) {
+        this.player.pause()
+      }
+    },
+    playerPlay() {
+      if (this.player) {
+        this.player.play()
+      }
+    },
+    playerDispose() {
+      if (this.player) {
+        this.player.dispose() // Removing Players,该方法会重置videojs的内部状态并移除dom
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+:deep(.vjs_video_3-dimensions) {
+  width: initial;
+  height: initial;
+}
+:deep(.video-js .vjs-tech),
+.base-video-player {
+  position: initial;
+  top: initial;
+  left: initial;
+  width: 100%;
+  height: 100%;
+  > video {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
